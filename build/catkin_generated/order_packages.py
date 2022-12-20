020a74f82c1f7f3504c5d0a369c19ac3de9b7439
mujinsong <44770623+zsm666@users.noreply.github.com>
2022-12-20 11:48:06 +0800

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/zsm/hello_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/zsm/eai_lidar_ws/devel;/home/zsm/hello_ws/devel;/home/zsm/slambook_ws/devel;/home/zsm/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/zsm/eai_lidar_ws/devel;/home/zsm/hello_ws/devel;/home/zsm/slambook_ws/devel;/home/zsm/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- build/solution/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zsm/hello_ws/src/solution
-# Build directory: /home/zsm/hello_ws/build/solution
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
